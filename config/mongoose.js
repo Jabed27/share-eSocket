60bad106aacde71bf8c08d7e909750777037398e
Jabed27 <zobaidulislamjabed@gmail.com>
2021-02-18 13:18:51 +0600

    changed

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,7 @@
 const mongo = require('mongoose');
-const baseURL = 'MONGOURL ADRESİ';
-mongo.connect(process.env.MONGODB_URI || baseURL,{useUnifiedTopology: true,  useCreateIndex: true, useNewUrlParser: true}).then((db)=>{
+//const baseURL = 'MONGOURL ADRESİ';
+require('dotenv').config();
+console.log(process.env.MONGODB_URI)
+mongo.connect(process.env.MONGODB_URI,{useUnifiedTopology: true,  useCreateIndex: true, useNewUrlParser: true}).then((db)=>{
     console.log('Connected MONGODB');
-}).catch((error)=> console.log('Not Connected'));
+}).catch((error)=> console.log('Not Connected',error));
